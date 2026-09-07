66077300e602c3102218f6f8b09bdbff32fabd43
7ckingSven <joristdave@gmail.com>
2026-09-07 18:16:02 +0800

    updated the QR code for the mobile app.

--- generate_qr.py
+++ generate_qr.py
@@ -4,7 +4,7 @@ from PIL import Image
 # APK download URL — now hosted on Flask backend for faster, more reliable downloads
 # Change this to your deployed domain in production (e.g., https://yourdomain.com/download-apk)
 # Change the URL "https://drive.google.com/uc?export=download&id= " 
-url = "https://drive.google.com/uc?export=download&id=1lSY3yVCyYAmGJN_r08cXPLfrWslAYi6I" 
+url = "https://drive.google.com/uc?export=download&id=1Coc0XMkEAKuR_oAJTwb3uz4kanWP5B6Z" 
 #url = "https://ecommerce-pos-8rsf.onrender.com/download-apk"
 # Create QR code with green branding
 
